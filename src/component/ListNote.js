8af0468775ee174c6734604b4ae62b60b4595768
Cristi <p.cristi0@yahoo,ro>
2020-01-03 16:42:51 +0200

    gradlew.bat clean in /android
    terminat login
    afisare lista items
    animatie wait
    
    todo:
    transmitere token la alta clasa/pagina
    salvare locala date
    synchronizare date cu server
    digrama
    harti sau email

--- src/component/ListNote.js
+++ src/component/ListNote.js
@@ -1,25 +1,26 @@
 import * as React from 'react';
-import {Button, Text, View, ActivityIndicator, FlatList} from 'react-native';
+import {Button, Text, View, ActivityIndicator, FlatList, TouchableOpacity} from 'react-native';
 import {getItems} from './Utils/Networking';
 
 export class DetailsScreen extends React.Component {
     state = {
         wait: true,
         loaded: false,
-        jsonArray: [{id:'',key:''}],
+        jsonArray: [{id: '', key: ''}],
     };
 
     constructor(props) {
         super(props);
         this.receiveData(); ///todo: de afisat animatia aici
     }
-     receiveData() {
+
+    receiveData() {
 
         getItems(global.tokenAplicatie).then((response) => response.json())
             .then((responseJson) => {
                 var arr = [];
                 for (var i = 0; i < responseJson.length; i++) {
-                    arr.push({key: responseJson[i]._id, nota: responseJson[i].text})
+                    arr.push({key: responseJson[i]._id, nota: responseJson[i].text});
                 }
                 this.setState({
                     jsonArray: arr,
@@ -42,13 +43,39 @@ export class DetailsScreen extends React.Component {
                         renderItem={({item}) => <Text style={styles.item}>{item.nota}</Text>}
                     />
                 </View>
-                <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <View style={styles.container3}>
                     {this.state.loaded ? <Text></Text> : <ActivityIndicator size="large"/>}
-                    <Text>Details Screen</Text>
-                    <Button
-                        title="Go to Details... modific "
-                        onPress={() => this.props.navigation.navigate('Details')}
-                    />
+                    <View style={styles.buttoncontainer}>
+                        <TouchableOpacity
+                            style={styles.buttoncontainer}
+                            onPress={() => this.props.navigation.navigate('Details')}>
+                            <Text style={styles.buttontext}>Adauga</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <Text style={styles.divider}></Text>
+                    <View style={styles.buttoncontainer}>
+                        <TouchableOpacity
+                            style={styles.buttoncontainer}
+                            onPress={() => this.props.navigation.navigate('Details')}>
+                            <Text style={styles.buttontext}>Sterge</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <Text style={styles.divider}></Text>
+                    <View style={styles.buttoncontainer}>
+                        <TouchableOpacity
+                            style={styles.buttoncontainer}
+                            onPress={() => this.props.navigation.navigate('Details')}>
+                            <Text style={styles.buttontext}>Harti</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <Text style={styles.divider}></Text>
+                    <View style={styles.buttoncontainer}>
+                        <TouchableOpacity
+                            style={styles.buttoncontainer}
+                            onPress={() => this.props.navigation.navigate('Details')}>
+                            <Text style={styles.buttontext}>Diagrama</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </>
         );
@@ -65,4 +92,35 @@ const styles = {
         fontSize: 18,
         height: 44,
     },
+    container2: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    container3: {
+        padding: 20,
+        flex: 1,
+        backgroundColor: '#ecf0f1',
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        height: 50,
+    },
+    textError: {
+        fontSize: 15,
+        color: 'red',
+    },
+    buttoncontainer: {
+        height: 50,
+        borderRadius: 50,
+        backgroundColor: '#1abc9c',
+        justifyContent: 'center',
+    },
+    buttontext: {
+        textAlign: 'center',
+        color: '#ecf0f1',
+        fontSize: 20,
+    },
+    divider:{
+        height: 10,
+    }
+
 };
